ce612e9a456f20fbc518959da6b8f168419ff60a
Aparna Hegde <aparna.hegde@33across.com>
2018-06-10 22:43:36 -0400

    FTP Client class

--- ftp.rb
+++ ftp.rb
@@ -4,13 +4,33 @@ def printLine(line)
   puts line
 end
 
-Net::FTP.open('ftp.test.com') do |ftp| 
-  ftp.login(user='foo', passwd='bar');
-
-  ftp.chdir('/ipblocklistv2');
-
-  ftp.gettextfile('test.csv') do |line| 
-    puts line;
+class FTPClient
+  ## attr_reader :host, :user, :password
+  def initialize(host, user, password)
+    @host = host
+    @user = user
+    @password = password
   end
+
+  def startConnection
+    ftp = NET::FTP.new(@host, @user, @password);
+
+    return ftp
+  end
+
+  def gettextfile(path, file, code)
+    ftp = startConnection()
+    ftp.chdir(path)
+
+    ftp.gettextfile(file) do |line|
+      code.call(line)
+    end
+
+  end
+
 end
+
+
+
+
 
